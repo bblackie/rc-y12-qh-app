c56ea06983f1e37c5b0a4db3699e4473c41746ae
quits <221059@trinityschools.nz>
2025-03-10 11:54:46 +1300

    trying to conect data base to app

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
-
+from flask import Flask,g
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
 
+DATABASE = 'cs_weapon.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+
+
 def index():
-    return "hello,world"
+    curser = get_db().curser()
+    sql ='SELECT * FROM contents'
+    curser.execute
+    results = curser.fetchall()
+    return str(results)
     
 if __name__=="__main__":
     app.run(debug=True)
